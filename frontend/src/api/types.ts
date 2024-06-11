6bc645dc051e6e1af9203f5dff3b18ed96f476f5
David Visotsky <visotskydavid@gmail.com>
2024-06-11 09:36:12 -0400

    remove vue and rename nextjs-frontend

--- frontend/src/api/types.ts
+++
@@ -1,4 +0,0 @@
-export interface apiResponse {
-  data: any
-  error?: any
-}
--- frontend/src/app/groups/[groupId]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from 'react'
-
-const GroupPage = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>
-}
-
-export default GroupPage
